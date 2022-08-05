5d2c575af67d87faf3bdb29eb68a52d9745af3b6
Xin Shang <shaxy010@mymail.unisa.edu.au>
2022-08-05 20:08:29 +0930

    ******_master branch_******

---
+++ java/src/javaTextCode/Main.java
@@ -0,0 +1,28 @@
+package javaTextCode;
+
+import methodAndTool.*;
+import frontEndView.HomePage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		AnswerWriteTxt A = new AnswerWriteTxt();
+		ViewChange VC = new ViewChange();
+		HomePage HP = new HomePage();
+		
+		System.out.println("------\tSTART\t------");
+		
+		A.writeAnswerInTxt();
+		
+		System.out.println("------ \tEND\t ------");
+		
+		
+		HP.createWindow ();
+		
+		
+		System.out.println("W: " + VC.getScreenWidthSize() + " H: " + VC.getScreenHeighSize());
+
+	}
+
+}
