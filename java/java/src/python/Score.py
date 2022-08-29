61cd0b41f56ebf4caa37193bda17b67289033517
OranZhao <zhazy100@mymail.unisa.edu.au>
2022-08-29 12:30:01 +0930

    Score calculation

--- java/java/src/python/Score.py
+++ java/java/src/python/Score.py
@@ -1,31 +1,79 @@
-from asyncio.windows_events import NULL
 import os
+
+from Text_Analysis import *
 
 _TotalScore = 0
 
 
 #compare the answer
 def _S_CompareAnswer(answer_1, answer_2):
-    if answer_1 == answer_2:
-        return True
-    else:
-        return False
-
-
-def S_countFunction():
-    print("")
-
-def S_countValuable():
-    print("")
-
+    answer_1 == answer_2
 
 def S_getTotalScore(userInput, Solution):
-    bIfcorrect = _S_CompareAnswer(userInput, Solution)
-    if(bIfcorrect == True):
-        _TotalScore = 100
-        return _TotalScore
-    else:
-        _TotalScore = 0
-        return _TotalScore
+    score = 0
+    score += (KeyWordCheck(question, userInput) + FunctionCheck(question, userInput) + ResultCheck(question, userInput) + VariableCheck(question, userInput))
+    return score
+
+# whether keyword exists
+def KeyWordCheck(question, userInput):
+    Keywordlist = question['keyword_Check'].split()
+    KWscore = question['KW_Score'].split()
+    userInputlist = userInput.replace('\n', ' ').split()
+    score = 0
+    for keyword, kws in zip(Keywordlist, KWscore):
+        if keyword in userInputlist:
+            score += float(kws)
+    
+    return score
+
+
+def FunctionCheck(question, userInput):
+    Functionlist = [f'\nprint({func.split(":")[0]})' for func in question['Function_Check'].split()]
+    answerlist = [func.split(":")[1] for func in question['Function_Check'].split()]
+    FScore = question['Function_Score'].split(',')
+    score = 0
+    for pfunc, ans, fss in zip(Functionlist, answerlist, FScore):
+        with open('tmp.py', 'w') as f:
+            f.write(userInput)
+            f.writelines(pfunc)
+        res = subprocess.check_output(['python', 'tmp.py']).decode('utf-8').split()
+        if _S_CompareAnswer(res[-1], ans):
+            score += float(fss)
+
+    os.remove('tmp.py')
+    return score
+
+
+def ResultCheck(question. userInput):
+    Resultlist = [f'\nprint({result.split(":")[0]})' for result in question['Result_Check'].split()]
+    answerlist = [result.split(":")[1] for result in question['Result_Check'].split()]
+    RScore = question['Result_Score'].split()
+    score = 0
+    for pres, ans, rss in zip(Resultlist, answerlist, RScore):
+        with open('tmp.py', 'w') as f:
+            f.write(userInput)
+            f.writelines(pres)
+        res = subprocess.check_output(['python', 'tmp.py']).decode('utf-8').split()
+        if _S_CompareAnswer(res[-1], ans):
+            score += float(rss)
+    os.remove('tmp.py')
+    return score
+
+
+def VariableCheck(question, userInput):
+    Variablelist = [f'\nprint({variable.split(":")[0]})' for variable in question['Variable_Value_Check'].split()]
+    valuelist = [variable.split(":")[1] for variable in question['Variable_Value_Check'].split()]
+    VScore = question['Variable_Value_Score'].split()
+    score = 0
+    for pval, val, vss in zip(Variablelist, valuelist, VScore):
+        with open('tmp.py', 'w') as f:
+            f.write(userInput)
+            f.writelines(pval)
+        res = subprocess.check_output(['python', 'tmp.py']).decode('utf-8').split()
+        if _S_CompareAnswer(res[-1], val):
+            score += float(vss)
+    os.remove('tmp.py')
+    return score
+
 
 
