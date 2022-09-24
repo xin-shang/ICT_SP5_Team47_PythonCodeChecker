10be0b520392a1f90b20b64105b0298914eb40b5
q847861249 <108392340+q847861249@users.noreply.github.com>
2022-09-24 17:24:55 +0930

    Connect mysql

--- project/src/JDBC/dbConnection/PythonCodeChecker_db.java
+++ project/src/JDBC/dbConnection/PythonCodeChecker_db.java
@@ -14,6 +14,9 @@ public class PythonCodeChecker_db {
     protected ResultSet res = null;
     String URL = "jdbc:sqlite:./src/sqlite/PYCodeChecker.db";
 
+
+    
+
     public void connectDB() {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -29,5 +32,27 @@ public class PythonCodeChecker_db {
         } catch (Exception e) {
             System.out.println(" disconnect fail");
         }
+    }
+
+
+    public static Connection get_connection() {
+        Connection connection = null;
+        String host="mysqlpythoncodechecker.cyi624ixm65n.us-east-2.rds.amazonaws.com";
+        String port="3306";
+        String db_name="PythonCodeChecker";
+        String username="admin";
+        String password="5281688.";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://"+""+host+":"+port+"/"+db_name+"", ""+username+"", ""+password+"");
+            if (connection != null) {   
+                System.out.println("Connection OK");
+            } else {
+                System.out.println("Connection Failed");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		return connection;
     }
 }
